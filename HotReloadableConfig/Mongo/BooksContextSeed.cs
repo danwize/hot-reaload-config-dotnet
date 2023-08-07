854e449f6b0ed462805ee3ea2bc564a44bd2845d
danwize <dbdavis3@gmail.com>
2023-08-07 19:48:25 +0000

    Added new book to seed

--- HotReloadableConfig/Mongo/BooksContextSeed.cs
+++ HotReloadableConfig/Mongo/BooksContextSeed.cs
@@ -19,7 +19,8 @@ public class BooksContextSeed
             new List<Book>
             {
                 new (Id: Guid.Parse("4e27009c-7df5-4fbf-b6cc-81924f35af46"), Title: "Configs in DotNet", Author: dan),
-                new (Id: Guid.Parse("5e27009c-7df5-4fbf-b6cc-81924f35af46"), Title: "DI in DotNet", Author: dan)
+                new (Id: Guid.Parse("5e27009c-7df5-4fbf-b6cc-81924f35af46"), Title: "DI in DotNet", Author: dan),
+                new (Id: Guid.Parse("5e27009c-7df5-4fbf-b6cc-81924f35af47"), Title: "Another Book?", Author: dan)
             });
     }
 }
